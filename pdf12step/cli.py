3e341eec97d7f921403bc9e454e9937d44b25493
Justin Quick <justquick@gmail.com>
2022-02-18 13:20:08 -0500

    use click groups for cli

--- pdf12step/cli.py
+++ pdf12step/cli.py
@@ -1,46 +1,15 @@
-import argparse
 import sys
 import os
 
+import click
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 from pdf12step.templating import Context, DIR
 from pdf12step.log import logger
 from pdf12step.config import Config
 from pdf12step.client import Client
-
-
-def add_arguments(parser):
-    """
-    Adds base arguments to all commands
-
-    :param argparse.ArgumentParser parser: Parser isinstance to add base args to
-    """
-    parser.add_argument('--config', '-c', action='append', help='Configs file for runtime vars. Can pass multiple to override options.')
-    parser.add_argument('--verbose', '-v', action='count', default=0, help='More verbose logging')
-    parser.add_argument('--logfile', default=None, help='Optional log file to wrie to')
-    return parser
-
-
-pdf_parser = argparse.ArgumentParser(description='Format meeting PDF')
-pdf_parser = add_arguments(pdf_parser)
-pdf_parser.add_argument('--output', '-o', default=None, help='Output file name to render to. Use "-" for stdout')
-pdf_parser.add_argument('--download', '-d', action='store_true', help='Download the assets before rendering. Produces up to date PDFs')
-pdf_parser.add_argument('--limit', '-l', type=int, help='Limit the rendering to this number of meetings')
-
-flask_parser = argparse.ArgumentParser(
-    description='Run Flask webapp for Development. This should never be run in production! You must restart this if you make any changes to code or asset files')
-flask_parser.add_argument('-a', '--address', default='0.0.0.0', help='The host interface address to bind to')
-flask_parser.add_argument('-p', '--port', type=int, default=5000, help='The port to bind to')
-
-client_parser = argparse.ArgumentParser(description='Download meeting data from a WordPress site with 12-step-meeting-list plugin.')
-client_parser = add_arguments(client_parser)
-client_parser.add_argument('-u', '--site-url', default=None, help='WordPress Site URL root')
-client_parser.add_argument('-f', '--format', default='json', choices=('json', 'csv'), help='Format of downloaded meeting data')
-client_parser.add_argument('-s', '--sections',  default=','.join(Client.sections), help='Comma separated list of sections to download')
-
-init_parser = argparse.ArgumentParser(description='Iniitialize your custom configuration interactively by answering a few questions')
-init_parser.add_argument('-o', '--output',  default='config.yaml', help='Filename to output your config to')
+from pdf12step.adict import AttrDict
+from pdf12step.__main__ import main
 
 
 def lister(value):
@@ -68,8 +37,87 @@ def prompt(name, title, default=None, cast=str):
     return {name: field}
 
 
-def init_main():
-    args = init_parser.parse_args()
+@click.group('12step')
+@click.option('--config', '-c', multiple=True,  help='Configs file for runtime vars. Can pass multiple to override options.')
+@click.option('--verbose', '-v', count=True, default=0, help='More verbose logging')
+@click.option('--logfile',  default=None, help='Optional log file to wrie to')
+@click.pass_context
+def cli(ctx, config, verbose, logfile):
+    ctx.ensure_object(dict)
+    ctx.obj.update(config=config, verbose=verbose, logfile=logfile)
+
+
+@cli.command()
+@click.option('--output', '-o', default=None, help='Output file name to render to. Use "-" for stdout')
+@click.option('--download', '-d', is_flag=True, help='Download the assets before rendering. Produces up to date PDFs')
+@click.option('--limit', '-l', type=int, help='Limit the rendering to this number of meetings')
+@click.pass_context
+def pdf(ctx, **kwargs):
+    """Formats meeting PDFs"""
+    ctx.obj.update(kwargs)
+    args = AttrDict(ctx.obj)
+    context = Context(args)
+    context.prerender()
+    content = context.pdf()
+    logger.info(f'Generated {len(content)//1000}KB of PDF content')
+    logger.info(f'Total meetings renderd: {len(context["meetings"])}')
+    outfile = args.output
+    if outfile is None:
+        outfile = open(context['now'].strftime('%B %Y Directory.pdf'), 'wb')
+    elif outfile == '-':
+        outfile = sys.stdout.buffer
+    else:
+        outfile = open(outfile, 'wb')
+    outfile.write(content)
+    outfile.close()
+    logger.info(f'Wrote to {outfile.name}')
+
+
+@cli.command()
+@click.option('-a', '--address', default='0.0.0.0', help='The host interface address to bind to')
+@click.option('-p', '--port', type=int, default=5000, help='The port to bind to')
+@click.pass_context
+def flask(ctx, **kwargs):
+    """
+    Run Flask webapp for Development.
+    This should never be run in production!
+    You must restart this if you make any changes to code or asset file
+    """
+    ctx.obj.update(kwargs)
+    args = AttrDict(ctx.obj)
+    from pdf12step.flask_app import app
+
+    os.environ['FLASK_APP'] = __name__
+    app.run(args.address, args.port)
+
+
+@cli.command()
+@click.option('-u', '--site-url', default=None, help='WordPress Site URL root')
+@click.option('-f', '--format', default='json', type=click.Choice(('json', 'csv')), help='Format of downloaded meeting data')
+@click.option('-s', '--sections',  default=','.join(Client.sections), help='Comma separated list of sections to download')
+@click.pass_context
+def download(ctx, **kwargs):
+    """
+    Downloads meeting data from your site_url
+    The site must be a WordPress site running the 12-step-meeting-list plugin.
+    """
+    ctx.obj.update(kwargs)
+    args = ctx.obj
+    site_url = args.pop('site_url')
+    config = Config().load(args)
+    Client(site_url if site_url is not None else config.site_url).download(*args['sections'].split(','), format=args['format'])
+
+
+@cli.command()
+@click.option('-o', '--output',  default='config.yaml', help='Filename to output your config to')
+@click.pass_context
+def init(ctx, **kwargs):
+    """
+    Prompts you to create a custom config
+    Iniitialize your custom configuration interactively by answering a few questions
+    """
+    ctx.obj.update(kwargs)
+    args = AttrDict(ctx.obj)
     sections = [
         ('Data Gathering', [
             ('site_url', 'Site URL running 12 Step Meeting WordPress plugin'),
@@ -116,35 +164,19 @@ def init_main():
     print(f'12step-pdf -c {args.output}')
 
 
-def client_main():
-    args = client_parser.parse_args().__dict__
-    site_url = args.pop('site_url')
-    config = Config().load(args)
-    Client(site_url if site_url is not None else config.site_url).download(*args['sections'].split(','), format=args['format'])
+@cli.command()
+@click.pass_context
+def shell(ctx, **kwargs):
+    """
+    Drops into an IPython shell
+    Contains the context, config and meetings instances
+    """
+    ctx.obj.update(kwargs)
+    args = AttrDict(ctx.obj)
+    main(args)
 
 
-def pdf_main():
-    args = pdf_parser.parse_args()
-    context = Context(args)
-    context.prerender()
-    content = context.pdf()
-    logger.info(f'Generated {len(content)//1000}KB of PDF content')
-    logger.info(f'Total meetings renderd: {len(context["meetings"])}')
-    outfile = args.output
-    if outfile is None:
-        outfile = open(context['now'].strftime('%B %Y Directory.pdf'), 'wb')
-    elif outfile == '-':
-        outfile = sys.stdout.buffer
-    else:
-        outfile = open(outfile, 'wb')
-    outfile.write(content)
-    outfile.close()
-    logger.info(f'Wrote to {outfile.name}')
-
-
-def flask_main():
-    from pdf12step.flask_app import app
-
-    args = flask_parser.parse_args()
-    os.environ['FLASK_APP'] = __name__
-    app.run(args.address, args.port)
+if __name__ == '__main__':
+    import ipdb
+    with ipdb.launch_ipdb_on_exception():
+        cli(obj={})
